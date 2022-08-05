08d987c4a6a65d55c738997bdf1990380f18fbe8
MD <dhmedy@googlemail.com>
2022-08-05 20:54:35 +0200

    mymesage

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@ let stringToSearch;
 let stringToExaminate;
 let output_txt;
 const txt = 'How inappropriate to call this planet Earth, when clearly it is Ocean.';
-
 let var1 = 'h';
 let var2 = 'eart';
 let var3 = 'moon';
+let myArray;
 
 const arrow_fn_wert_assign_values = (stringToSearch, stringToExaminate) => {
     stringToSearch = document.getElementById('idStr').value;
@@ -21,26 +21,33 @@ const arrow_fn_show_pos = (stringToSearch, stringToExaminate) => {
     console.log(ausgabe);
     document.getElementById('pOutput').innerHTML = ausgabe;
     // Second part of the exercie "h", "earth", "moon"
-    let myArray = [var1, var2, var3];
-    console.log(myArray, txt);
-    arrow_fn_show_pos2(myArray, txt);
+
 }
 
 // To be continued
+myArray = [var1, var2, var3];
+
+// Call the func
+
 
 const arrow_fn_show_pos2 = (myArray, txt) => {
+    console.log(myArray, txt);
     let array2 = myArray;
     console.log(array2);
     console.log(myArray[var1]);
-    for (let index; index <= array2.length; i++) {
-        let position_of_stringToSearch = stringToExaminate.indexOf(array2[index]);
+    let position_of_stringToSearch;
+
+    for (let index = 0; index <= array2.length; index++) {
+        position_of_stringToSearch = txt.indexOf(array2[index]);
         ausgabe = `Position of ${array2[index]} in ${txt} ist = ${position_of_stringToSearch}`;
         console.log(ausgabe);
-        document.getElementById('pOutput2').innerHTML = document.getElementById('pOutput2').innerHTML + ausgabe + '^br°';
+        document.getElementById('pOutput2').innerHTML = document.getElementById('pOutput2').innerHTML + ausgabe + '<br>' + '<hr>';
     }
+
+
 }
 
 
 
-
+arrow_fn_show_pos2(myArray, txt);
 
